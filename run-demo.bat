cd3f3437fd61b6320fec4a083065529ec608fa16
Andre Stubbe <andrestubbe@proton.me>
2026-06-05 13:59:22 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -1,5 +1,7 @@
-﻿@echo off
+@echo off
+chcp 65001 >nul
+cd /d "%~dp0"
 echo ðŸš€ Running Demo...
-call mvn -q -f examples/Demo/pom.xml compile
+call mvn -f examples/Demo/pom.xml compile
 call java --enable-native-access=ALL-UNNAMED -cp "target\fastglob-0.1.0.jar;examples\Demo\target\classes" fastglob.Demo
 pause
